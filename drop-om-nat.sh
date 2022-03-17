d319c70e8d6feef9172dafeb4c9af0e733ed8ee2
kolyan-r <kolyan-r@yandex.ru>
2022-03-17 14:48:12 +0300

    Version: 1.0.1

--- drop-om-nat.sh
+++ drop-om-nat.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-SCRIPT_PATH="$( cd "$(dirname "$1")" ; pwd -P )"
+SCRIPT_PATH="$(
+  cd "$(dirname "$1")"
+  pwd -P
+)"
 ROUTES_PATH=${SCRIPT_PATH}/routes.json
 
 DEFAULT_ID="om-nat-M4RdjRw2ZhT"
 CUSTOM_ID=$(jq -r '.id' ${ROUTES_PATH})
 
-if [ "${CUSTOM_ID}" = "null" ]
-then
-    ID="${DEFAULT_ID}"
+if [ "${CUSTOM_ID}" = "null" ]; then
+  ID="${DEFAULT_ID}"
 else
-    ID="${CUSTOM_ID}"
+  ID="${CUSTOM_ID}"
 fi
 
 echo "Drop OM NAT: ${ID}"
--- reload-om-nat.sh
+++ reload-om-nat.sh
@@ -2,30 +2,70 @@
 
 set -e
 
-SCRIPT_PATH="$( cd "$(dirname "$1")" ; pwd -P )"
-ROUTES_PATH=${SCRIPT_PATH}/routes.json
+ROUTES_PATH=$1
 
-DEFAULT_ID="om-nat-M4RdjRw2ZhT"
-CUSTOM_ID=$(jq -r '.id' ${ROUTES_PATH})
+NODE_NAME=$2
 
-if [ "${CUSTOM_ID}" = "null" ]
-then
-    ID="${DEFAULT_ID}"
+echo "NODE_NAME: ${NODE_NAME}"
+
+DEFAULT_CONTEXT_ID="M4RdjRw2ZhT"
+CUSTOM__CONTEXT_ID=$(jq -r '.id' ${ROUTES_PATH})
+
+if [ "${CUSTOM__CONTEXT_ID}" = "null" ]; then
+  CONTEXT_ID="${DEFAULT_CONTEXT_ID}"
 else
-    ID="${CUSTOM_ID}"
+  CONTEXT_ID="${CUSTOM__CONTEXT_ID}"
 fi
 
-bash ${SCRIPT_PATH}/drop-om-nat.sh
+echo "CONTEXT_ID: ${CONTEXT_ID}"
+
+CURRENT_RULES=$(iptables-save | { grep -oP "${CONTEXT_ID}_[a-z0-9]{40}" || :; })
 
 get_obj_prop() {
-   local data=$1
-   local key=$2
-   echo ${data} | base64 --decode | jq -r ${key}
+  local DATA=$1
+  local KEY=$2
+  echo ${DATA} | base64 --decode | jq -r "${KEY}"
 }
 
-ROUTES=$(jq -r '.routes' ${ROUTES_PATH})
+drop_rule() {
+  local RULE_ID=$1
+  echo "Drop rule: ${RULE_ID}"
+  iptables-save | grep -v "${RULE_ID}" | iptables-restore
+}
 
-for row in $(echo "${ROUTES}" | jq -r '.[] | @base64'); do
+drop_from_current_rule() {
+  local RULE_ID=$1
+  CURRENT_RULES=$(echo "${CURRENT_RULES}" | { grep -v "${RULE_ID}" || :; })
+}
+
+has_rule() {
+  local RULE_ID=$1
+  echo $(iptables-save | { grep "${RULE_ID}" || :; })
+}
+
+get_rule_hash() {
+  local RULE_DATA=$1
+  echo -n "${RULE_DATA}" | sha1sum | awk '{print $1}'
+}
+
+add_rule() {
+  local RULE_DATA=$1
+  local RULE_HASH=$(get_rule_hash "${RULE_DATA}")
+  local RULE_ID="${CONTEXT_ID}_${RULE_HASH}"
+  local HAS_RULE=$(has_rule "${RULE_ID}")
+  if [ "${HAS_RULE}" == "" ]; then
+    echo "Append rule (${RULE_ID}): ${RULE_DATA}"
+    iptables ${RULE_DATA} -m comment --comment "${RULE_ID}"
+  else
+    echo "Ignore rule (${RULE_ID}): ${RULE_DATA}"
+    drop_from_current_rule "${RULE_ID}"
+  fi
+}
+
+ROUTES=$(jq -r ".nodes.${NODE_NAME}.routes" ${ROUTES_PATH})
+
+if [ "${ROUTES}" != "null" ]; then
+  for row in $(echo "${ROUTES}" | jq -r '.[] | @base64'); do
     NAME=$(get_obj_prop ${row} '.name')
     echo "Add rules to ${NAME}"
     FROM_ADDRESS=$(get_obj_prop ${row} '.from.address')
@@ -33,20 +73,26 @@ for row in $(echo "${ROUTES}" | jq -r '.[] | @base64'); do
     TO_INTERFACE=$(get_obj_prop ${row} '.to.interface')
     PREROUTING=$(get_obj_prop ${row} '.prerouting')
     POSTROUTING_DEST=""
-    if [ "${TO_ADDRESS}" = "null" ]
-    then
-        POSTROUTING_DEST="MASQUERADE"
+    if [ "${TO_ADDRESS}" = "null" ]; then
+      POSTROUTING_DEST="MASQUERADE"
     else
-        POSTROUTING_DEST="SNAT --to-source ${TO_ADDRESS}"
+      POSTROUTING_DEST="SNAT --to-source ${TO_ADDRESS}"
     fi
-    POSTROUTING_RULE="-t nat -A POSTROUTING -s ${FROM_ADDRESS} -o ${TO_INTERFACE} -m comment --comment "${ID}" -j ${POSTROUTING_DEST}"
-    echo "Postrouting rule: iptables ${POSTROUTING_RULE}"
-    iptables ${POSTROUTING_RULE}
-    if [ "${PREROUTING}" = true ] ; then
-        PREROUTING_RULE="-t nat -A PREROUTING -p tcp -d ${TO_ADDRESS} -m comment --comment "${ID}" -j DNAT --to-destination ${FROM_ADDRESS%/*}"
-        echo "Prerouting rule: iptables ${PREROUTING_RULE}"
-        iptables ${PREROUTING_RULE}
+    POSTROUTING_RULE="-t nat -A POSTROUTING -s ${FROM_ADDRESS} -o ${TO_INTERFACE} -j ${POSTROUTING_DEST}"
+    add_rule "${POSTROUTING_RULE}"
+    if [ "${PREROUTING}" = true ]; then
+      PREROUTING_RULE="-t nat -A PREROUTING -p tcp -d ${TO_ADDRESS} -j DNAT --to-destination ${FROM_ADDRESS%/*}"
+      add_rule "${PREROUTING_RULE}"
     fi
-done
+  done
+else
+  echo "Routes not found"
+fi
 
-iptables -t raw -I PREROUTING -i fwbr+  -m comment --comment "${ID}" -j CT --zone 1
+add_rule "-t raw -I PREROUTING -i fwbr+ -j CT --zone 1"
+
+if [ "${CURRENT_RULES}" != "" ]; then
+  while IFS= read -r LINE; do
+    drop_rule "${LINE}"
+  done <<<"${CURRENT_RULES}"
+fi
